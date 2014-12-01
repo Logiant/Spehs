d7d0fc845e1dfca587e75224840f40513c20cc9c
Logiant <beaverl@msoe.edi>
2014-12-01 15:47:19 -0600

    Click to fire, extinguisher decays
    Look out, fire!

--- Assets/Scripts/ExtinguisherScript.cs
+++ Assets/Scripts/ExtinguisherScript.cs
@@ -3,9 +3,30 @@ using System.Collections;
 
 public class ExtinguisherScript : MonoBehaviour {
 
+	public bool firing;
+	public float propellant = 10f;
+	public float decay = 1f; //units per second
+	ParticleRenderer particles;
+
+	void Start() {
+		particles = GameObject.Find ("Particles").GetComponent<ParticleRenderer> ();
+	}
+
+	void Update() {
+		if (firing) {
+			propellant -= decay * Time.deltaTime;
+		}
+		firing = firing && (propellant > 0);
+		if (firing) {
+
+		} if (Input.GetButtonDown ("Fire1")) {
+			firing = !firing;
+		}
+		particles.enabled = firing;
+	}
+
 	void OnTriggerEnter(Collider other) {
-		if (other.CompareTag ("Fire") ){
-			Debug.Log (other.tag);
+		if (firing && other.CompareTag ("Fire") ){
 			Destroy(other.gameObject);
 		}
 	}
