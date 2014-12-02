2b3b3c08600676a95e20a8cb794f8963d8c028fa
Logan <beaverl@msoe.edu>
2014-12-02 16:05:41 -0600

    Updated extinguisher again
    Shoots better now

--- Assets/Scripts/ExtinguisherScript.cs
+++ Assets/Scripts/ExtinguisherScript.cs
@@ -6,14 +6,14 @@ public class ExtinguisherScript : MonoBehaviour {
 	public bool firing;
 	public float propellantLeft = 10f;
 	public float decay = 1f; //units per second
-	public float muzzleVelocity = 5f;
+	public float muzzleVelocity = 7f;
 	public GameObject propellant;
 	public CharacterController player;
 
 	public Transform spawn;
 
 	private float currentRot = 0;
-	private float dRot = 5;
+	private float dRot = 3;
 	private float maxRot = 10;
 //	ParticleRenderer particles;
 
@@ -26,7 +26,7 @@ public class ExtinguisherScript : MonoBehaviour {
 			propellantLeft -= decay * Time.deltaTime;
 			currentRot += dRot;
 			if (currentRot > maxRot)
-				currentRot = -maxRot;
+				currentRot -= 2*maxRot;
 			Quaternion rot = Quaternion.Euler (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + currentRot, transform.rotation.eulerAngles.z);
 			GameObject p = (GameObject) Instantiate (propellant, spawn.position, rot);
 			p.rigidbody.velocity = p.transform.rotation * new Vector3(0, 0, muzzleVelocity) + player.velocity;
