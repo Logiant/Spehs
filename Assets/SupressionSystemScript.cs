886921b9871064cab643583be946d2c4a98438e5
Logan <beaverl@msoe.edu>
2014-12-15 21:11:32 -0600

    Created initial fire suppression script
    Side note, I don't know how to spell suppression!

---
+++ Assets/SupressionSystemScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SupressionSystemScript : MonoBehaviour {
+	
+	public float maxPropellant = 10f;
+	public bool running;
+	public float drainRate = 0.75f; //drain rate per spout
+	public float propellant;
+
+	SupressorHeadScript[] spouts;
+
+
+	// Use this for initialization
+	void Start () {
+		spouts = GetComponentsInChildren<SupressorHeadScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (running)
+			propellant -= (drainRate * spouts.Length * Time.deltaTime);
+
+
+		running = running && propellant > 0;
+
+		for (int i = 0; i < spouts.Length; i++) {
+			spouts[i].spouting = running;
+		}
+	}
+}
---
+++ Assets/SupressorHeadScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SupressorHeadScript : MonoBehaviour {
+
+	public bool spouting;
+	public GameObject propellant;
+
+	Transform[] spouts;
+	// Use this for initialization
+	void Start () {
+		spouts = transform.GetComponentsInChildren<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spouting) {
+			for (int i = 1; i < spouts.Length; i ++) {
+				GameObject p = (GameObject)Instantiate (propellant, spouts[i].position, spouts[i].rotation);
+				//rotate += 45 degrees
+				float yRot = Random.Range (-45, 46);
+				p.transform.Rotate (new Vector3(0, yRot, 0));
+				p.rigidbody.velocity = p.transform.rotation * new Vector3(0, 0, Random.Range (0, 7f));
+
+			}
+
+		}
+	}
+}
