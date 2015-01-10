11d668411db00ae7e2cf5be143ff089d9c725f60
Logiant <beaverl@msoe.edi>
2015-01-10 12:03:45 -0600

    Added placeholder scripts

---
+++ Assets/HealthScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+	float maxHealth;
+	float health;
+
+	public HealthScript(float max, float current) {
+		maxHealth = max;
+		health = current;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		health = Mathf.Clamp (health, 0, maxHealth);
+	}
+
+	public void damage(float amt) {
+		health -= amt;
+	}
+
+	public float getHealth() {
+		return health;
+	}
+}
--- Assets/Scripts/WeaponsBayScript.cs
+++ Assets/Scripts/WeaponsBayScript.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(HealthScript))]
 public class WeaponsBayScript : MonoBehaviour {
 
-
+	HealthScript health;
 	public float victoryTime = 240; //seconds before enemy ship is destroyed
 	public bool broken;
 	
