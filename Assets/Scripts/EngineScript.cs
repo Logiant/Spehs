8d8b0590965ccff3e6944cfff7f4596c07dc84a4
Ian Mahuta <mahutai@msoe.edu>
2015-01-10 12:05:19 -0600

    Added shield and engine scripts
    Look, I'm doing stuff!

--- Assets/Scripts/EngineScript.cs
+++ Assets/Scripts/EngineScript.cs
@@ -1,20 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShieldGenScript : MonoBehaviour {
+public class EngineScript : MonoBehaviour {
 
 	int health;
 	int currentHealth;
-	bool shieldsUp;
+	bool isBroken;
+	bool isMalfunctioning;
+
 	// Use this for initialization
 	void Start () {
-		shieldsUp = true;
-		health = 15;
-		currentHealth = health;
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		shieldsUp = (health != 0);
+	
 	}
 }
